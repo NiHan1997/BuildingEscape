109864773bfaf98380f0356bf21026c28180eff2
NiHanGame <371429357@qq.com>
2019-12-28 10:35:10 +0800

    03 场景完善与旋转开门逻辑测试.

---
+++ Source/BuildingEscape/DoorOpen.cpp
@@ -0,0 +1,35 @@
+﻿// Light's NiHan in 2019.
+
+
+#include "DoorOpen.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UDoorOpen::UDoorOpen()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorOpen::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator rotation(0.0f, -70.0f, 0.0f);
+	GetOwner()->SetActorRotation(rotation);
+}
+
+
+// Called every frame
+void UDoorOpen::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/BuildingEscape/DoorOpen.h
@@ -0,0 +1,28 @@
+// Light's NiHan in 2019.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DoorOpen.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UDoorOpen : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorOpen();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
