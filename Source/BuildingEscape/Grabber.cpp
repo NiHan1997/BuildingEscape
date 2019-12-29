44dca10cf0916d654f0fb96ef6af530420e1fe10
NiHanGame <371429357@qq.com>
2019-12-29 16:20:39 +0800

    06 物品拾取基础准备, 切换GameMode以及DefaultPawn.

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,45 @@
+﻿// Light's NiHan in 2019.
+
+
+#include "Grabber.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 获取当前视口的位置和旋转.
+	FVector playerLocation;
+	FRotator playerRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT playerLocation, OUT playerRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("当前视口的位置是: %s, 旋转是: %s"),
+		*playerLocation.ToString(), *playerRotation.ToString());
+}
+
